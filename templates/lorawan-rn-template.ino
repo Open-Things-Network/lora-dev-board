6b2bb1b940141e5186e32ddfd7aaa58a6a427175
mkubas <mariusz.r.kubas@gmail.com>
2020-04-22 18:52:54 +0200

    Add templates

---
+++ templates/lorawan-rn-template.ino
@@ -0,0 +1,122 @@
+/*
+Application template for lora-dev-board with Arduino Pro Mini
+-------------------------------------------------------------
+Board version:       1.0
+Board configuration: RN2483
+*/
+#include <TheThingsNetwork.h>
+#include <SoftwareSerial.h>
+#include <LowPower.h>
+
+#define JUST_SEND_HELLO
+#define SAVE_ENERGY
+#define JOIN_MODE_OTAA
+
+
+// ------- outputs and interfaces (see lora-dev-board scheme)
+#define USER_LED 2 
+#define VOUT 3 
+SoftwareSerial userSerial(8, 9); // RX, TX
+SoftwareSerial rnSerial(6, 7); // RX, TX
+
+// ------- LoRaWAN configuration
+
+#define freqPlan TTN_FP_EU868
+
+#ifdef JOIN_MODE_OTAA
+  // OTAA mode - set AppEUI and AppKey
+  const char *appEUI = "0000000000000000";
+  const char *appKey = "00000000000000000000000000000000";
+#else
+  // ABP mode - set DevAddr and session keys
+  const char *devAddr = "00000000";
+  const char *nwkSKey = "00000000000000000000000000000000";
+  const char *appSKey = "00000000000000000000000000000000";
+#endif
+
+// RN2483
+TheThingsNetwork ttn(rnSerial, Serial, freqPlan);
+
+// ------- intervals, buffers, etc.
+
+// data send interval in ms
+const unsigned TX_INTERVAL = 60000;
+
+// data buffer
+#ifdef JUST_SEND_HELLO
+  static uint8_t hello[] = "Hello!";
+#else
+  CayenneLPP lppdata(51);
+#endif
+
+// -------
+
+void getData()
+{
+#ifndef JUST_SEND_HELLO
+  lppdata.reset();
+  // here you can read data from sensors and prepare buffer to send
+#endif
+}
+
+void setup() 
+{
+  Serial.begin(9600);
+  userSerial.begin(9600);
+  
+  pinMode(USER_LED, OUTPUT);
+  pinMode(VOUT, OUTPUT);
+  
+  rnSerial.begin(9600);
+  ttn.wake();
+
+  ttn.onMessage(message);
+
+#ifdef JOIN_MODE_OTAA
+  Serial.println("--- STATUS");
+  ttn.showStatus();
+  Serial.println("--- JOIN");
+  ttn.join(appEUI, appKey);
+#else
+  Serial.println("-- PERSONALIZE");
+  ttn.personalize(devAddr, nwkSKey, appSKey);
+  Serial.println("--- STATUS");
+  ttn.showStatus();
+#endif
+}
+
+void loop() 
+{
+  Serial.println("--- SEND");
+  digitalWrite(USER_LED, HIGH);
+#ifdef JUST_SEND_HELLO
+  ttn.sendBytes(hello, sizeof(hello) - 1);
+#else
+  getData();
+  ttn.sendBytes(lppdata.getBuffer(), lppdata.getSize());
+#endif
+  digitalWrite(USER_LED, LOW);
+
+#ifdef SAVE_ENERGY
+  ttn.sleep(TX_INTERVAL);
+  Serial.flush();
+  for (int i = 0; i < int(TX_INTERVAL/8000); i++) 
+  {
+    LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
+  }
+  ttn.wake();
+#else
+  delay(TX_INTERVAL);
+#endif
+}
+
+void message(const uint8_t *payload, size_t size, port_t port)
+{
+  Serial.println("--- MESSAGE");
+  Serial.print("Received " + String(size) + " bytes on port " + String(port) + ": ");
+  for (int i = 0; i < size; i++)
+  {
+    Serial.print(payload[i], HEX);
+  }
+  Serial.println();
+}
